3b688ee10cc30ba3edb7b0467a44c619a638f4e7
Michael Savarese <geomajor56@gmail.com>
2015-05-17 16:15:34 -0400

    adding good data to github

--- monitor/views.py
+++ monitor/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomePage(generic.TemplateView):
+    template_name = "index.html"
+
+
+class AboutPage(generic.TemplateView):
+    template_name = "about.html"
